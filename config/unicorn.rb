390bd08826588a706d1f69d714f54aa219458532
Rusty Klophaus <rklophaus@gmail.com>
2014-10-29 17:48:26 -0400

    Add unicorn config.

---
+++ config/unicorn.rb
@@ -0,0 +1,26 @@
+# config/unicorn.rb
+
+worker_processes Integer(ENV['WEB_CONCURRENCY'] || 4)
+timeout Integer(ENV['WEB_TIMEOUT'] || 30)
+preload_app true
+
+before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+  end
+end
+
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
+  end
+
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+  end
+end
