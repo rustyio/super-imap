3e8b851d302b393bf33bb8ef3f0dc4e544bb0e3d
Rusty Klophaus <rklophaus@gmail.com>
2014-12-05 17:05:30 -0500

    Create a custom AdminUser view.

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -1,21 +1,30 @@
 ActiveAdmin.register AdminUser do
   permit_params :email, :password, :password_confirmation
   menu priority: 100
+  config.sort_order = "email_asc"
+  config.filters = false
 
   index do
-    selectable_column
-    id_column
-    column :email
-    column :current_sign_in_at
-    column :sign_in_count
-    column :created_at
-    actions
+    column :tag do |obj|
+      link_to obj.email, admin_admin_user_path(obj)
+    end
+    column :last_sign_in_at
   end
 
-  filter :email
-  filter :current_sign_in_at
-  filter :sign_in_count
-  filter :created_at
+  show do |obj|
+    panel "Details" do
+      attributes_table_for obj do
+        row :id
+        row :email
+        row :sign_in_count
+        row :current_sign_in_at
+        row :current_sign_in_ip
+        row :last_sign_in_at
+        row :last_sign_in_ip
+        row :created_at
+      end
+    end
+  end
 
   form do |f|
     f.inputs "Admin Details" do
@@ -25,5 +34,4 @@ ActiveAdmin.register AdminUser do
     end
     f.actions
   end
-
 end
