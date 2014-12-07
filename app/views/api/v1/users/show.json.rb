fe71a72818760eff4d969cc978afb651c1c2666d
Rusty Klophaus <rklophaus@gmail.com>
2014-12-07 13:40:31 -0500

    Properly render json.

--- app/views/api/v1/users/show.json.rb
+++ app/views/api/v1/users/show.json.rb
@@ -3,4 +3,4 @@
   :email          => @user.email,
   :connect_url    => new_users_connect_url(@user.signed_request_params),
   :disconnect_url => new_users_disconnect_url(@user.signed_request_params)
-}
+}.to_json
