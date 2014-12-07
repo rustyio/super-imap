53cab783c488d55ef45a61d13bc145ff9cb1e1c5
Rusty Klophaus <rklophaus@gmail.com>
2014-12-07 15:03:00 -0500

    Add delayed_jobs process.

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
-web: bundle exec unicorn -p $PORT -c ./config/unicorn.rb
-imap_client: rake imap:client
+web:         bundle exec unicorn -p $PORT -c ./config/unicorn.rb
+worker:      env QUEUE=worker bundle exec rake jobs:work
+imap_client: bundle exec rake imap:client
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141207191800) do
+ActiveRecord::Schema.define(version: 20141207200312) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,22 @@ ActiveRecord::Schema.define(version: 20141207191800) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "delayed_jobs", force: true do |t|
+    t.integer  "priority",   default: 0, null: false
+    t.integer  "attempts",   default: 0, null: false
+    t.text     "handler",                null: false
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
 
   create_table "imap_daemon_heartbeats", force: true do |t|
     t.string   "tag"
