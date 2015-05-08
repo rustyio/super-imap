6f0e76328cccff61f38f5b4493140f2a4265a398
Guille Carlos <guille@bitpop.in>
2015-05-08 07:07:28 -0700

    Instead of calling the constants require files

--- app/processes/imap_client.rb
+++ app/processes/imap_client.rb
@@ -2,6 +2,8 @@ class ImapClient
   VERSION="1.0.0"
 end
 
+require 'imap_daemon_heartbeat'
+require 'call_new_mail_webhook'
 require 'imap_client/rendezvous_hash'
 require 'imap_client/maybe'
 require 'imap_client/process_uid'
